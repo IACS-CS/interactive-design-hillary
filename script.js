fe5a54a36f3ef747daa01e24d1c8411bc474825f
Hillary-N-N <hillary.naggenda@innovationcharter.org>
2025-12-08 23:55:09 +0000

    need help

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 console.log("Hello, Interactive Graphic Design!");
 // This is the main JavaScript file for the Interactive Graphic Design project.
+// grab all the flipper cards on the page
+const allFlippers = document.querySelectorAll(".flipper");
+
+// loop through each flipper card
+for (let i = 0; i < allFlippers.length; i++) {
+  const currentFlipper = allFlippers[i]; // grab the current flipper
+  
+  // add a click listener to each flipper
+  currentFlipper.addEventListener(
+    "click", // when the user clicks
+    function () {
+      // do this:
+      currentFlipper.classList.toggle("flipped"); // add or remove the "flipped" class
+    }
+  );
+}
